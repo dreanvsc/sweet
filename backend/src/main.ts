56f4d93c80794e2a51706075163c4dbc7f77fb48
dreanvsc <andre23mar01@gmail.com>
2026-05-19 23:16:11 +0100

    blindar o servidor com cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,17 @@ const passport = require('passport');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(); 
+  app.enableCors({
+    // AQUI ESTÁ A TUA LISTA VIP DE SITES AUTORIZADOS
+    origin: [
+      'http://localhost:3000', // A tua oficina local 1
+      'http://localhost:3001', // A tua oficina local 2
+      'https://sweet-sooty.vercel.app', // O teu link atual do Vercel (muda se for outro)
+      'https://sweetdrop.vercel.app' // O link bonito que criaste no Vercel
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // 1. Iniciar as Sessões (A "Memória")
   app.use(

06d8fca785cd344c1e01f6f205db945d36b52c60
dreanvsc <andre23mar01@gmail.com>
2026-05-19 23:27:47 +0100

    tirar N

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // 🔥 Isto manda o Vercel ignorar os avisos e publicar o site na mesma!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // 🔥 Ignora também os avisos de formatação
-    ignoreDuringBuilds: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  devIndicators: {
+    buildActivity: false, // ISTO DESLIGA O "N"
   },
 };
 

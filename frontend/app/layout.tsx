5b67ead527011c659970e8ce0f8acf14bb835cef
dreanvsc <117177393+dreanvsc@users.noreply.github.com>
2026-06-16 12:03:16 +0100

    Change body background color to dark

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="pt"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-[#0b0b0d]">
         
         {/* O CONTEÚDO PRINCIPAL DO TEU SITE */}
         <main className="flex-1">
